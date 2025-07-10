b5803fbaabd6b665dee2af5fe8c84e785e68445a
Wilhelm Rosental <wilhelm.rosental@ik.me>
2025-07-10 23:21:17 +0200

    init

---
+++ src/main/java/dev/nathanhallouin/devboost/DevBoostApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanhallouin.devboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanhallouin/devboost/DevBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanhallouin.devboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
